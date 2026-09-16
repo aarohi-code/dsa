abb5fb342015a234c91ad07362cae2e96f58aec7
Aarohi Mahajan <aarohimahajan@Aarohis-MacBook-Pro.local>
2026-09-16 15:38:08 -0700

    Merge 2 sorted lists

---
+++ src/main/java/lists/MergeTwoSortedLists.java
@@ -0,0 +1,51 @@
+    class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+
+  }
+  
+class MergeTwoSortedLists {
+    public static void main(String[] args) {
+        System.out.println("Testing...");
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(2);
+        list2.next.next = new ListNode(3);
+
+        ListNode result = mergeTwoLists(list1, list2);
+        System.out.println("result: "+ result.val);
+        //print result
+        while(result!=null){
+            System.out.print(result.val+ " ");
+            result = result.next;
+        }
+    }
+    public static ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode tail = new ListNode();
+        ListNode dummy = tail;
+
+        while(list1!=null && list2 !=null){
+            if(list1.val<list2.val){
+                tail.next = list1;
+                list1=list1.next;
+            } else{
+                tail.next = list2;
+                list2=list2.next;
+            }
+            tail = tail.next;
+        }
+        if(list1!=null){
+            tail.next = list1;
+        } else{
+            tail.next = list2;
+        }
+
+        return dummy.next;
+    }
+}
